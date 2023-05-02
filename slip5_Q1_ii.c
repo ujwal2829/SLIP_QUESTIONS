e0f222c392c3ec14ecea6ff343ca3d596e414206
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2023-05-02 16:54:51 +0530

    slip5 Q1(2)

--- slip5_Q1_ii.c
+++ slip5_Q1_ii.c
@@ -2,11 +2,21 @@
 #include<string.h>
 void main()
 {
+    int i;
     char str1[50],str2[50];
     printf("Enter string one\t");
     scanf("%s",str1);
     printf("Enter string two\t");
     scanf("%s",str2);
+
+    i = strcmp(str1,str2);
+    if(i==0)
+        printf("BOTH STRINGS ARE EQUAL\n");
+    else if(i>0)
+        printf("STRING 2 ARE GREATER\n");
+    else
+        printf("STRING 1 IS GREATER\n");
+    
     printf("String before copying\n");
     printf("string 1%s\n",str1);
     printf("string 2%s\n",str2);
@@ -15,5 +25,6 @@ void main()
     strcpy(str1,str2);
     printf("string 1%s\n",str1);
     printf("string 2%s\n",str2);
+
 
 }
---
+++ test.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+struct item
+{
+    int code;
+    char name[50];
+    int price;
+}I[50];
+void main()
+{
+    int n,i,max_price,z;
+    char item_name[10];
+    printf("ENTER TEH NUMBER OF ENTRIES: \t");
+    scanf("%d",&n);
+
+    for ( i = 0; i < n; i++)
+    {
+        printf("ENTER THE ITEM CODE: \t");
+        scanf("%d",&I[i].code);
+        printf("ENTER THE NAME OF THE ITEM: \t");
+        scanf("%s",I[i].name);
+        printf("ENTER THE PRICE OF THE ITEM: \t");
+        scanf("%d",&I[i].price);
+    }
+    printf("\n-------------------------SEARCH DETAILS--------------------\n");
+    max_price = I[0].price;
+    for ( i = 0; i < n; i++)
+    {
+        if (I[i].price>max_price)
+            max_price = I[i].price;
+    }
+    for ( i=0; i<n; i++)
+    {
+        if(I[i].price == max_price)
+            printf("\nITEM WITH MAX PRICE\n");
+            printf("CODE = %d\nNAME = %s\nPRICE = %d\n",I[i].code,I[i].name,I[i].price);
+    }
+    printf("ENTER THE NAME OF ITME TO BE SEARCHED\t");
+    scanf("%s",item_name);
+    
+    for ( i = 0; i < n; i++)
+    {
+        z = strcmp(I[i].name,item_name);
+    }
+    printf("Z = %d",z);
+        if (z == 0)
+            printf("ITEM FOUND\n");
+        else
+            printf("INVALID ITEM NAME\n");
+}
